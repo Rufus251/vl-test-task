e989ee67df23bbb892e2ca5455c1789c152e0322
Rufus <vladimerrybalkin@gmail.com>
2023-04-12 18:27:32 +1000

    Добавлен адаптив

--- to-do/src/components/ViewTask.vue
+++ to-do/src/components/ViewTask.vue
@@ -142,7 +142,6 @@ export default{
         font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
     }
 
-
     $backBtnBgColor: #ffffff;
     $editBtnBgColor: #0091DC;
     
@@ -156,11 +155,14 @@ export default{
     }
     .button{
         width: 100%;
+
         display: flex;
         justify-content: center;
         align-items: center;
+
+        gap: 15px;
+
         input{
-            margin-left: 15px;
             padding: 15px;
             border: 0;
             border-radius: 6px;  
@@ -176,6 +178,9 @@ export default{
         .back{
             color: $backTextColor;
             background-color: $backBtnBgColor;
+            
+
+
         }
         .edit{
             color: $editTextColor;
@@ -188,20 +193,25 @@ export default{
     $HeaderFontColor: #808080;
     $ContentFontColor: #464646;
     .taskData{
+        background-color: #ffffff;
+        padding: 1px 25px 25px 25px;
+
+        margin-top: 30px;
 
         ul{
             list-style: none;
 
             li{
-                margin-top: 30px;
+                margin-top: 25px;
 
-                
                 h2{
                     font-size: $FontSize;
                     color: $HeaderFontColor;
                     font-weight: 400;
                 }
                 p{
+                    word-break: break-word;
+
                     font-size: $FontSize;
                     color: $ContentFontColor;
                     font-weight: 400;
@@ -234,5 +244,61 @@ export default{
             opacity: 0.9;
             }
         }
+    }
+
+    @media (min-width: 768px) {
+        .wrapper{
+            width: 90%;
+        }
+
+        .button{
+            
+        }
+        
+        .wrapper{
+            display: grid;
+            gap: 10px;
+
+            grid-template-columns: (80% , );
+            grid-template-rows: (100px, );
+
+            .button{
+                margin-top: 40px;
+
+                justify-content: left;
+
+                grid-column-start: 1;
+                grid-column-end: 2;
+
+                grid-row-start: 1;
+                grid-row-end: 2;
+            }
+
+            .taskData{
+                grid-column-start: 1;
+                grid-column-end: 3;
+
+                grid-row-start: 2;
+                grid-row-end: 3;
+            }
+
+            .deleteBtn{
+                margin-top: 40px;
+
+                grid-column-start: 2;
+                grid-column-end: 3;
+
+                grid-row-start: 1;
+                grid-row-end: 2;
+
+                justify-content: right;
+
+                input{
+                    margin: 0;
+                }
+            }
+        }
+
+
     }
 </style>
